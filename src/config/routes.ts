5e4cf1f88e8bb291023f91fbec85956d1701604a
MARUMARU310505 <gemelos310505@gmail.com>
2026-06-07 17:55:16 -0600

    feat: add new images and implement header, footer, and hero section components

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -3,6 +3,33 @@ export const ROUTE = {
     title: "Inicio",
     path: "/",
   },
+  MEN: {
+    title: "Men",
+    path: "/",
+  },
+  WOMEN: {
+    title: "Women",
+    path: "/",
+  },
+  FEATURED: {
+    title: "Featured",
+    path: "/",
+  },
+  SHOP: {
+    title: "Shop",
+    path: "/",
+  },
+  HISTORY: {
+    title: "History",
+    path: "/",
+  },
+  CONTACT: {
+    title: "Contact",
+    path: "/",
+  },
 };
 
 export const ROUTES = [ROUTE.HOME];
+
+export const NAV_COLLECTIONS = [ROUTE.MEN, ROUTE.WOMEN, ROUTE.FEATURED];
+export const NAV_ROUTES = [ ROUTE.HISTORY, ROUTE.CONTACT];
