d58540df888ec75c2e304fff2828b2f5bebda0a0
MARUMARU310505 <gemelos310505@gmail.com>
2026-06-09 21:34:33 -0600

    refactor: migrate ProductCard component from Astro to TypeScript and update imports across sections

---
+++ src/components/section-components/productCard.tsx
@@ -0,0 +1,60 @@
+import { ROUTE } from "@/config/routes";
+import { formatPrice } from "@/lib/utils";
+import type { IProductCard } from "@/types/pages-cms";
+
+export const ProductCard = ({
+  name,
+  price,
+  tag,
+  img,
+  imgHover,
+  size,
+  slug,
+}: IProductCard) => {
+  return (
+    <a
+      href={`${ROUTE.SHOP.path}/${slug}`}
+      className="w-full h-full flex flex-col gap-2.5 group"
+    >
+      <div className="w-full aspect-4/4">
+        <div className="w-full h-full relative">
+          <img
+            className="w-full h-full object-cover absolute inset-0"
+            src={img}
+            alt={name}
+          />
+          <img
+            className="w-full h-full object-cover absolute inset-0 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+            src={imgHover}
+            alt={`${name} hover`}
+          />
+        </div>
+      </div>
+      <div className="w-full h-full flex flex-col justify-between gap-2.5">
+        <div className="w-full flex max-sm:flex-col justify-end sm:justify-between max-md:text-sm">
+          <p className="w-full font-light line-clamp-3 uppercase sm:mr-2">
+            {name}
+          </p>
+          <p className="min-w-fit text-website-orange font-mozilla font-medium text-end">
+            {formatPrice(price)}
+          </p>
+        </div>
+        <div className="w-full font-mozilla font-medium uppercase text-xs md:text-sm relative">
+          <p className="w-full">{tag}</p>
+          <div className="w-full h-full bg-white flex justify-center gap-3 absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+            {size.map((item: string) => (
+              <div
+                key={item}
+                className="flex justify-center aspect-square border-b border-black"
+              >
+                <p className="">{item}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default ProductCard;
--- src/components/sections/products-section.tsx
+++ src/components/sections/products-section.tsx
@@ -1,5 +1,35 @@
-export default function ProductsSection(){
-    return (
-        <section> hola mundo </section>
-    )
-}
+import type { IProductCard } from "@/types/pages-cms";
+import ProductCard from "../section-components/productCard";
+
+export const ProductsSection = ({ products }: { products: IProductCard[] }) => {
+  return (
+    <section className="w-full py-14 lg:py-20 flex justify-center">
+      <div className="w-full max-wrapper ">
+        <div className="w-full grid grid-cols-4 justify-between items-stretch gap-5">
+          <div className="col-span-1 w-full h-full border">
+            
+          </div>
+          <div className="col-span-3 w-full  ">
+            <div className="w-full grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-8 xl:gap-10">
+              {products.map((product) => {
+                return (
+                  <ProductCard
+                    img={product.img}
+                    imgHover={product.imgHover}
+                    name={product.name}
+                    price={product.price}
+                    tag={product.tag}
+                    size={product.size}
+                    slug={product.slug}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProductsSection;
