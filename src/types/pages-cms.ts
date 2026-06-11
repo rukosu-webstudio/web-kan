cf38af62ff3805614c4dcaad3dde598f0e7c5656
MARUMARU310505 <gemelos310505@gmail.com>
2026-06-10 21:49:55 -0600

    feat: add pages cms types and required some inputs

--- src/types/pages-cms.ts
+++ src/types/pages-cms.ts
@@ -1,21 +1,28 @@
 export interface IProductCard {
-  img: string;
-  imgHover: string;
-  name: string;
-  price: number;
-  size: string[];
-  slug: string;
-  tag: string;
-}
-
-export interface IProduct {
-  img: string;
+  img?: string;
   imgHover?: string;
   name: string;
   price: number;
-  size: string[];
+  size?: string[];
   slug: string;
-  tag: string;
+  tag?: string;
+}
+
+export interface IProduct {
+  name: string;
+  slug: string;
+  price: number;
+  size: string[];
+
+  tag?: string;
+  color?: string;
+
   description?: string;
+  moreDetails?: {
+    title: string;
+    description: string;
+  }[];
+
+  img?: string;
   gallery?: string[];
 }
